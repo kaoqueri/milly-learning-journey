5ed8df5442c61b0029ec49188aa6e9b57c9ed007
kaoqueri <millynapas@gmail.com>
2025-09-02 12:02:56 +0700

    Finished module 1

--- cpp_Revision/print.cpp
+++ cpp_Revision/print.cpp
@@ -12,5 +12,33 @@ int main() {
     cout << "Hello World!" << endl; // New line
     cout << "My name is Milly."; // No new line
 
+    // --- Variables ---
+    // Integer
+    int age = 20;
+    cout << age << endl;
+
+    // Double
+    double decimal = 0.17;
+    cout << decimal << endl;  
+
+    // Boolean (Case sensitive)
+    bool TheTruth = true;
+    cout << boolalpha << TheTruth << endl; // boolalpha makes output 'true' or 'false'
+
+    bool TheSecondTruth = true;
+    cout << TheSecondTruth << endl; // Without boolalpha, the output is 1 or 0
+    
+    //String
+    string words = "I got peach.";
+    cout << words << endl;
+
+    /* 
+    --- Variable Naming Rules ---
+    + Start with a letter or underscore
+    + Remainder of variable name is letters, numbers, or underscores
+    + Cannot use keywords
+    + Case sensitive
+    */
+    
     return 0;
 }
